a3d9d880d1b538db055f0fb5ebf00f933a2ae528
Yasith Chandula <76646376+yasithchandula@users.noreply.github.com>
2022-03-23 20:20:51 +0530

    pharmacist consumer & producer

--- PharmacistProducer/src/pharmacistproducer/Activator.java
+++ PharmacistProducer/src/pharmacistproducer/Activator.java
@@ -2,21 +2,23 @@ package pharmacistproducer;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+	private ServiceRegistration serviceRegistration;
 
-	static BundleContext getContext() {
-		return context;
-	}
-
+	@Override
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+		System.out.println("Pharmacist Producer started");
+		PharmacistService pharmacistService = new PharmacistServiceImpl();
+		serviceRegistration = bundleContext.registerService(PharmacistService.class.getName(), pharmacistService, null);
 	}
 
+	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+		System.out.println("Pharmacist Producer stopped");
+		serviceRegistration.unregister();
 	}
 
 }
